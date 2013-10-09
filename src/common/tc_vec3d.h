10bd0a4eccaa59ca7ae0e7a7aac818a2b190ae91
Alejandro <ale11@stanford.edu>
2013-10-09 09:53:52 -0700

    Unrolled loops in tc_vec3d.h

--- src/common/tc_vec3d.h
+++ src/common/tc_vec3d.h
@@ -13,10 +13,7 @@ void divide_vec3d(double * v1,const double scalar);
 
 inline double vecDotVec3d(const double * v1, const double * v2) {
 
-  double dotproduct = 0.0;
-
-  for (int i = 0; i < 3; i++)
-    dotproduct += v1[i]*v2[i];
+  double dotproduct = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
 
   return(dotproduct);
 }
@@ -27,8 +24,9 @@ inline double normVec3d(double * v1) {
 
   mag = sqrt(mag);
 
-  for (int i = 0; i < 3; i++)
-    v1[i] /= mag;
+  v1[0] /= mag;
+  v1[1] /= mag;
+  v1[2] /= mag;
 
   return mag;
 }
@@ -39,16 +37,29 @@ inline double normVec3d(double *vNorm, const double *v1) {
 
   mag = sqrt(mag);
 
-  for (int i = 0; i < 3; i++)
-    vNorm[i] = v1[i]/mag;
+  vNorm[0] = v1[0]/mag;
+  vNorm[1] = v1[1]/mag;
+  vNorm[2] = v1[2]/mag;
 
   return mag;
 }
 
 inline void vecMinVec3d(double *vRes, const double *v1, const double *v2) {
 
-  for (int i = 0; i < 3; i++)
-    vRes[i] = v1[i] - v2[i];
+  vRes[0] = v1[0] - v2[0];
+  vRes[1] = v1[1] - v2[1];
+  vRes[2] = v1[2] - v2[2];
+}
+
+inline void matMultMatR3(double (*res)[3], double (*m1)[3], double (*m2)[3])  // m*m matrices
+{
+  for (int i=0; i<3; i++)
+  for (int j=0; j<3; j++)
+  {
+    res[i][j] = 0.0;
+    for (int k=0; k<3; k++)
+      res[i][j] += m1[i][k]*m2[k][j];
+  }
 }
 
 inline void matMultMatR5(double (*res)[5], double (*m1)[5], double (*m2)[5])  // m*m matrices
